dfbd840a23ade100e192cda7935ad1778e1fdd09
Diana <113461954+Lapidusa@users.noreply.github.com>
2025-05-29 14:36:50 +0300

    edit buf pydanticv2

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,4 @@
-import os
-
-from pydantic_settings  import  BaseSettings
+from pydantic_settings import BaseSettings
 from pydantic import Field, field_validator
 from dotenv import load_dotenv, find_dotenv
 
@@ -12,13 +10,14 @@ class Settings(BaseSettings):
     DATABASE_URL_HOST: str
     DATABASE_URL: str | None = None
 
-    SMS_API_KEY: str =  os.environ.get('SMS_API_KEY')
-    SMS_API_URL: str = os.environ.get('SMS_API_URL')
-    JWT_SECRET_KEY: str = os.environ.get('JWT_SECRET_KEY')
-    ADMIN_PHONE: str = os.getenv("ADMIN_PHONE")
+    SMS_API_KEY: str
+    SMS_API_URL: str
+    JWT_SECRET_KEY: str
+    ADMIN_PHONE: str
 
     @field_validator("DATABASE_URL", mode="before")
-    def pick_db_url(self, v, info):
+    @staticmethod
+    def pick_db_url(v, info):
         env = info.data.get("ENV", "development").lower()
         if env == "production":
             return info.data["DATABASE_URL_HOST"]
@@ -27,6 +26,5 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-
 
 settings = Settings()
