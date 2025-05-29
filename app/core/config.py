8f42b7a6d5b51f12ff09590a4f4fb278d14c96f4
Diana <113461954+Lapidusa@users.noreply.github.com>
2025-05-29 15:07:04 +0300

    edit config

--- app/core/config.py
+++ app/core/config.py
@@ -6,22 +6,12 @@ load_dotenv(find_dotenv())
 
 class Settings(BaseSettings):
     ENV: str = Field(default="development")
-    DATABASE_URL_LOCAL: str
-    DATABASE_URL_HOST: str
     DATABASE_URL: str | None = None
 
     SMS_API_KEY: str
     SMS_API_URL: str
     JWT_SECRET_KEY: str
     ADMIN_PHONE: str
-
-    @field_validator("DATABASE_URL", mode="before")
-    @staticmethod
-    def pick_db_url(v, info):
-        env = info.data.get("ENV", "development").lower()
-        if env == "production":
-            return info.data["DATABASE_URL_HOST"]
-        return info.data["DATABASE_URL_LOCAL"]
 
     class Config:
         env_file = ".env"
